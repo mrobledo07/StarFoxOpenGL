a692f14e1a6a91fdc652907a327d4ec9f17a4735
Miguel Robledo <mrobledo027@gmail.com>
2024-11-23 20:36:03 +0100

    starship and background loaded

---
+++ app/src/main/java/com/example/starfox/Background.java
@@ -0,0 +1,119 @@
+package com.example.starfox;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.opengl.GLUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+import java.nio.ShortBuffer;
+
+import javax.microedition.khronos.opengles.GL10;
+
+public class Background {
+    private FloatBuffer vertexBuffer; // Buffer for vertex-array
+    private FloatBuffer texBuffer;    // Buffer for texture-coords-array (NEW)
+
+
+    private float[] vertices;
+
+    private float[] texCoords = { // Texture coords
+            0.0f, 1.0f,  // A. left-bottom (NEW)
+            1.0f, 1.0f,  // B. right-bottom (NEW)
+            0.0f, 0.0f,  // C. left-top (NEW)
+            1.0f, 0.0f   // D. right-top (NEW)
+    };
+    int[] textureIDs = new int[1];   // Array for 1 texture-ID (NEW)
+    private short[] indices = { 0, 1, 2, 1, 3, 2 };
+    private ShortBuffer indexBuffer;
+
+    public Background(float aspect) {
+        this.setAspect(aspect);
+
+        // Setup texture-coords-array buffer, in float. An float has 4 bytes (NEW)
+        ByteBuffer tbb = ByteBuffer.allocateDirect(texCoords.length * 4);
+        tbb.order(ByteOrder.nativeOrder());
+        texBuffer = tbb.asFloatBuffer();
+        texBuffer.put(texCoords);
+        texBuffer.position(0);
+
+        ByteBuffer ibb = ByteBuffer.allocateDirect(indices.length * 2);
+        ibb.order(ByteOrder.nativeOrder());
+        indexBuffer = ibb.asShortBuffer();
+        indexBuffer.put(indices);
+        indexBuffer.position(0);
+    }
+
+    public void setAspect(float aspect) {
+        if (aspect > 1.0f) {
+            vertices = new float[]{
+                    -aspect*15,  -aspect*3, 0.0f, // A. left-bottom
+                    aspect * 15,  -aspect*3, 0.0f,  // B. right-bottom
+                    -aspect * 15, aspect*3, 0.0f,  // C. left-top
+                    aspect * 15, aspect*3, 0.0f    // D. right-top
+            };
+        } else {
+            vertices = new float[]{
+                    -aspect * 10, -aspect * 15, 0.0f, // A. left-bottom
+                    aspect * 10, -aspect * 15, 0.0f,  // B. right-bottom
+                    -aspect * 10, aspect * 15, 0.0f,  // C. left-top
+                    aspect * 10, aspect * 15, 0.0f    // D. right-top
+            };
+        }
+
+        // Reasignar el buffer de vértices
+        ByteBuffer vbb = ByteBuffer.allocateDirect(vertices.length * 4);
+        vbb.order(ByteOrder.nativeOrder()); // Usar el orden de bytes nativo
+        vertexBuffer = vbb.asFloatBuffer();
+        vertexBuffer.put(vertices);         // Copiar los datos al buffer
+        vertexBuffer.position(0);           // Rewind
+    }
+
+    public void loadTexture(GL10 gl, Context context) {
+        gl.glGenTextures(1, textureIDs, 0); // Generate texture-ID array
+
+        gl.glBindTexture(GL10.GL_TEXTURE_2D, textureIDs[0]);   // Bind to texture ID
+        // Set up texture filters
+        gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MIN_FILTER, GL10.GL_NEAREST);
+        gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MAG_FILTER, GL10.GL_LINEAR);
+
+        // Construct an input stream to texture image "res\drawable\nehe.png"
+        InputStream istream = context.getResources().openRawResource(R.raw.corneria_bg);
+
+        Bitmap bitmap;
+        try {
+            // Read and decode input as bitmap
+            bitmap = BitmapFactory.decodeStream(istream);
+        } finally {
+            try {
+                istream.close();
+            } catch(IOException e) { }
+        }
+
+        // Build Texture from loaded bitmap for the currently-bind texture ID
+        GLUtils.texImage2D(GL10.GL_TEXTURE_2D, 0, bitmap, 0);
+        bitmap.recycle();
+    }
+
+    public void draw(GL10 gl) {
+        gl.glDisable(GL10.GL_DEPTH_TEST); // Deshabilitar profundidad para que el fondo no bloquee otros objetos
+        gl.glDisable(GL10.GL_LIGHTING);
+        gl.glBindTexture(GL10.GL_TEXTURE_2D, textureIDs[0]);
+        gl.glEnable(GL10.GL_TEXTURE_2D);
+        gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
+        gl.glVertexPointer(3, GL10.GL_FLOAT, 0, vertexBuffer);
+        gl.glEnableClientState(GL10.GL_TEXTURE_COORD_ARRAY);
+        gl.glTexCoordPointer(2, GL10.GL_FLOAT, 0, texBuffer);
+        gl.glDrawElements(GL10.GL_TRIANGLES, indices.length, GL10.GL_UNSIGNED_SHORT, indexBuffer);
+        gl.glDisableClientState(GL10.GL_TEXTURE_COORD_ARRAY);
+        gl.glDisableClientState(GL10.GL_VERTEX_ARRAY);
+        gl.glDisable(GL10.GL_TEXTURE_2D);
+        gl.glBindTexture(GL10.GL_TEXTURE_2D, 0);
+        gl.glEnable(GL10.GL_LIGHTING);
+        gl.glEnable(GL10.GL_DEPTH_TEST); // Rehabilitar profundidad
+    }
+}
--- app/src/main/java/com/example/starfox/MyOpenGLRenderer.java
+++ app/src/main/java/com/example/starfox/MyOpenGLRenderer.java
@@ -5,32 +5,119 @@ import javax.microedition.khronos.opengles.GL10;
 
 import android.content.Context;
 import android.opengl.GLSurfaceView.Renderer;
-import com.example.starfox.Loader;
+import android.opengl.GLU;
 
 public class MyOpenGLRenderer implements Renderer {
 
 	public Context context;
+	public Object3D object3D;
+	public Background background;
+	private float width, height;
+	private int angle = 0;
+	float Z = 1;
+	private Light light;
+
+
 	public MyOpenGLRenderer(Context context){
 		this.context = context;
+		this.object3D = new Object3D(context, R.raw.starfox_ship);
+		this.background = new Background((float)width/height);
 	}
+
 
 
 	@Override
 	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-		gl.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-		gl.glEnable(GL10.GL_DEPTH_TEST);
-		gl.glEnable(GL10.GL_TEXTURE_2D);
-		int backgroundTexture = Loader.loadTexture(gl, context, "corneria_bg.png");
-		Loader.loadObj("starfox_ship.blend");
+		gl.glClearColor(1.0f, 1.0f, 1.0f, 1.0f);  // Set color's clear-value to black
+		gl.glClearDepthf(1.0f);            // Set depth's clear-value to farthest
+		gl.glEnable(GL10.GL_DEPTH_TEST);   // Enables depth-buffer for hidden surface removal
+		gl.glDepthFunc(GL10.GL_LEQUAL);    // The type of depth testing to do
+		gl.glEnable(GL10.GL_BLEND);
+		gl.glBlendFunc(GL10.GL_SRC_ALPHA, GL10.GL_ONE_MINUS_SRC_ALPHA);
+
+		gl.glHint(GL10.GL_PERSPECTIVE_CORRECTION_HINT, GL10.GL_NICEST);  // nice perspective view
+		gl.glShadeModel(GL10.GL_SMOOTH);   // Enable smooth shading of color
+		gl.glDisable(GL10.GL_DITHER);      // Disable dithering for better performance
+
+
+		//Enable Lights
+		gl.glEnable(GL10.GL_LIGHTING);
+
+		// Enable Normalize
+		gl.glEnable(GL10.GL_NORMALIZE);
+		// You OpenGL|ES initialization code here
+		// ......
+
+		// Load background
+		background.loadTexture(gl, context);
+
+		light = new Light(gl, GL10.GL_LIGHT0);
+		light.setPosition(new float[]{0.0f, 0f, 1, 0.0f});
+
+		light.setAmbientColor(new float[]{0.1f, 0.1f, 0.1f});
+		light.setDiffuseColor(new float[]{1, 1, 1});
 	}
 
 	@Override
 	public void onSurfaceChanged(GL10 gl, int width, int height) {
+		if (height == 0) height = 1;   // To prevent divide by zero
+		this.width = width;
+		this.height = height;
+		float aspect = (float) width / height;
 
+		background.setAspect(aspect);
+
+		// Set the viewport (display area) to cover the entire window
+		gl.glViewport(0, 0, width, height);
+
+		// Setup perspective projection, with aspect ratio matches viewport
+		gl.glMatrixMode(GL10.GL_PROJECTION); // Select projection matrix
+		gl.glLoadIdentity();                 // Reset projection matrix
+		// Use perspective projection
+		GLU.gluPerspective(gl, 60, aspect, 0.1f, 100.f);
+
+
+		gl.glMatrixMode(GL10.GL_MODELVIEW);  // Select model-view matrix
+		gl.glLoadIdentity();                 // Reset
 	}
 
 	@Override
 	public void onDrawFrame(GL10 gl) {
+		// Clear color and depth buffers using clear-value set earlier
+		gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
+		gl.glLoadIdentity();
 
+		light.setPosition(new float[]{this.getZ(), 0f, 5, 0});
+		GLU.gluLookAt(gl, 0, 0, 10, 0f, 0f, 0f, 0f, 1f, 0f);
+
+		// Draw background
+		background.draw(gl);
+
+
+		gl.glPushMatrix();// Reset model-view matrix ( NEW )
+		gl.glRotatef(angle, 1, 1, 1);
+		object3D.draw(gl);                   // Draw triangle ( NEW )
+		gl.glPopMatrix();
+
+
+
+		angle = (angle + 2)%360;
+
+	}
+
+	public float getHeight() {
+		return this.height;
+	}
+
+	public float getWidth() {
+		return this.width;
+	}
+
+	public float getZ() {
+		return Z;
+	}
+
+	public void setZ(float z) {
+		this.Z = z;
 	}
 }
